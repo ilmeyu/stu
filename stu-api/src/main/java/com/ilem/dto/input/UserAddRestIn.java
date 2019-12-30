db097a4a6f0e62eccb799ac0ab92360ab460d71d
yuwk <yuwk@asiainfo.com>
2019-12-30 11:33:22 +0800

    新增-新增用户接口-补充入参

--- stu-api/src/main/java/com/ilem/dto/input/UserAddRestIn.java
+++ stu-api/src/main/java/com/ilem/dto/input/UserAddRestIn.java
@@ -1,13 +1,33 @@
 package com.ilem.dto.input;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
 import java.io.Serializable;
 
 /**
  * @author yuwenkai
  * @date 2019/11/13 2:49 下午
  **/
+@Getter
+@Setter
+@Accessors(chain = true)
+@ApiModel(value = "新增用户")
 public class UserAddRestIn implements Serializable {
 	private static final long serialVersionUID = 3337780421417983107L;
 
+	@ApiModelProperty(value = "用户名")
+	private String name;
 
+	@ApiModelProperty(value = "密码")
+	private String password;
+
+	@ApiModelProperty(value = "邮件")
+	private String email;
+
+	@ApiModelProperty(value = "手机号")
+	private String phone;
 }
